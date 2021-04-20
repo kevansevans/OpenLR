b88f9ec593433e18dd0a0b51ab01e80f9e51ed41
Kaelan <kbeevans@gmail.com>
2021-04-20 13:17:25 -0600

    Started .sol Reader work

---
+++ library/src/hxlr/file/AMF0Reader.hx
@@ -0,0 +1,111 @@
+package hxlr.file;
+
+import haxe.io.Bytes;
+
+/**
+ * ...
+ * @author Kaelan
+ * 
+ * Code to read the .sol format for line rider tracks
+ */
+ 
+class AMF0Reader 
+{
+
+	var data:Bytes;
+	
+	var pos:Int = 0;
+	var size:Int;
+	var header:Int;
+	
+	var object:Dynamic = {};
+	
+	var loadedSOL:Beta2Save;
+	
+	public function new(_sol:Bytes) 
+	{
+		data = _sol;
+		
+		header = data.get(pos) << 8 | data.get(pos += 1);
+		size = data.get(pos += 1) << 24 | data.get(pos += 1) << 16 | data.get(pos += 1) << 8 | data.get(pos += 1);
+		
+		if (size > data.length) {
+			trace("Internal size value greater than file size!");
+			return;
+		}
+		
+		var tcso:String = data.getString(pos += 1, 4);
+		if (tcso.toUpperCase() != "TCSO") {
+			trace("Not a valid .sol/AMF0 file!", tcso);
+			return;
+		}
+		pos += 11;
+		
+		var saveNameSize = data.get(pos);
+		var saveNameField = data.getString(pos += 1, saveNameSize);
+		if (saveNameField != "savedLines") {
+			trace(saveNameSize, saveNameField);
+			trace("Unable to locate savedLines!");
+			return;
+		}
+		pos += 4;
+		
+		while (true) {
+			
+			var itemNameSize = data.get(pos) << 8 | data.get(pos += 1);
+			var itemNameField = data.getString(pos += 1, itemNameSize);
+			
+			switch (itemNameField) {
+				
+				case trackList :
+					trace("Tracklist found!");
+				default :
+			}
+			
+			if (pos >= size) break;
+		}
+		
+	}
+	
+	public function loadSOLTrack(_index:Int):Beta2Track
+	{
+		return { };
+	}
+}
+
+typedef Beta2Save = {
+	var trackList:Array<Beta2Track>;
+}
+
+typedef Beta2Track = {
+	
+}
+
+enum abstract ValidFields(String) to String {
+	var data;
+	var label;
+	var startLine;
+	var trackList;
+}
+
+enum abstract DataType(Int) to Int
+{
+	var NUMBER;
+	var BOOLEAN;
+	var STRING;
+	var OBJECT;
+	var MOVIECLIP;
+	var NULL;
+	var UNDEFINED;
+	var REFERENCE;
+	var EMCA_ARRAY;
+	var OBJECTEND;
+	var STRICT_ARRAY;
+	var DATE;
+	var LONG_STRING;
+	var UNSUPPORTED;
+	var RECORDSET;
+	var XML_DOCUMENT;
+	var TYPED_OBJECT;
+	var AVMPLUS_OBJECT;
+}
