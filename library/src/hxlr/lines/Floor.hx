589cd2f842b76dc4cdeabe55d2d3e8be101e7c45
Kaelan <kbeevans@gmail.com>
2021-08-31 11:59:26 -0600

    Cleaned up decompiled code
    and added comments to explain behavior

--- library/src/hxlr/lines/Floor.hx
+++ library/src/hxlr/lines/Floor.hx
@@ -8,7 +8,7 @@ import hxlr.math.geom.Point;
  * ...
  * @author Kaelan
  */
-class Floor extends LineObject 
+class Floor extends LineObject
 {
 	
 	public function new(_start:Point, _end:Point, _shift:Bool = false, _limMode:Int = 0) 
@@ -22,18 +22,24 @@ class Floor extends LineObject
 	
 	override public function collide(_point:ContactPoint):Void 
 	{
-		var _loc5 = _point.pos.x - start.x;
-		var _loc6 = _point.pos.y - start.y;
-		var _loc4 = nx * _loc5 + ny * _loc6;
-		var _loc7 = (_loc5 * dx + _loc6 * dy) * invSqrDistance;
-			
+		var xDist = _point.pos.x - start.x;
+		var yDist = _point.pos.y - start.y;
+		var vDist = nx * xDist + ny * yDist; 					//How far perpendicularily the point is away from the line
+		var hDist = (xDist * dx + yDist * dy) * invSqrDistance; //How far along parallel the point is along the line
+		
+		//is the point moving "towards" the line?
 		if (_point.dir.x * nx + _point.dir.y * ny > 0)
 		{
-			if (_loc4 > 0 && _loc4 < zone && _loc7 >= limStart && _loc7 <= limEnd) {
-				_point.pos.x = _point.pos.x - _loc4 * nx;
-				_point.pos.y = _point.pos.y - _loc4 * ny;
-				_point.vel.x = _point.vel.x + ny * _point.friction * _loc4 * (_point.vel.x < _point.pos.x ? 1 : -1);
-				_point.vel.y = _point.vel.y - nx * _point.friction * _loc4 * (_point.vel.y < _point.pos.y ? -1 : 1);
+			//is the point within the line's range?
+			if (vDist > 0 && vDist < zone && hDist >= limStart && hDist <= limEnd) {
+				
+				//Move the point onto the line
+				_point.pos.x = _point.pos.x - vDist * nx;
+				_point.pos.y = _point.pos.y - vDist * ny;
+				
+				//Change it's speed to reflect it "colliding"
+				_point.vel.x = _point.vel.x + ny * _point.friction * vDist * (_point.vel.x < _point.pos.x ? 1 : -1);
+				_point.vel.y = _point.vel.y - nx * _point.friction * vDist * (_point.vel.y < _point.pos.y ? -1 : 1);
 			}
 		}
 		
--- library/src/hxlr/lines/Slow.hx
+++ library/src/hxlr/lines/Slow.hx
@@ -15,23 +15,29 @@ class Slow extends LineObject
 	{
 		super(_start, _end, _shift, _lim);
 		
-		type = ACCEL;
+		type = SLOW;
 		
 		tangible = true;
 	}
 	
 	override public function collide(_point:ContactPoint):Void 
 	{
-		var _loc5 = _point.pos.x - start.x;
-		var _loc6 = _point.pos.y - start.y;
-		var _loc4 = nx * _loc5 + ny * _loc6;
-		var _loc7 = (_loc5 * dx + _loc6 * dy) * invSqrDistance;
-			
+		var xDist = _point.pos.x - start.x;
+		var yDist = _point.pos.y - start.y;
+		var vDist = nx * xDist + ny * yDist;					//How far perpendicularily the point is away from the line
+		var hDist = (xDist * dx + yDist * dy) * invSqrDistance; //How far along parallel the point is along the line
+		
+		//is the point moving "towards" the line?
 		if (_point.dir.x * nx + _point.dir.y * ny > 0)
 		{
-			if (_loc4 > 0 && _loc4 < zone && _loc7 >= limStart && _loc7 <= limEnd) {
-				_point.pos.x = _point.pos.x - _loc4 * nx;
-				_point.pos.y = _point.pos.y - _loc4 * ny;
+			//is the point within the line's range?
+			if (vDist > 0 && vDist < zone && hDist >= limStart && hDist <= limEnd) {
+				
+				//Move the point onto the line
+				_point.pos.x = _point.pos.x - vDist * nx;
+				_point.pos.y = _point.pos.y - vDist * ny;
+				
+				//Change it's speed to reflect it "colliding" and decelerate
 				_point.vel.x = _point.vel.x - slowby * ny * accConst * (_point.vel.x > _point.pos.x ? 1 : -1);
 				_point.vel.y = _point.vel.y + slowby * nx * accConst * (_point.vel.y > _point.pos.y ? -1 : 1);
 			}
