98149afbe0de3de8c88604ac129aa40dca932a80
Kaelan <kbeevans@gmail.com>
2021-09-27 13:19:28 -0600

    Remove Kha stuff

--- khafile.js
+++
@@ -1,6 +0,0 @@
-let project = new Project('OpenLR');
-project.addAssets('Assets/**');
-project.addShaders('Shaders/**');
-project.addSources('Sources');
-project.addLibrary('hxlr');
-resolve(project);
