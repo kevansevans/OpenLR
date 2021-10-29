c89464e51ea501758287a73131dae3ca4519e37a
Kaelan <kbeevans@gmail.com>
2021-10-29 14:50:43 -0600

    Working on new menu

---
+++ heaps/src/components/ui/MenuMain.hx
@@ -0,0 +1,168 @@
+package components.ui;
+
+import components.io.SaveLoad;
+import h2d.Object;
+import haxe.ui.components.CheckBox;
+import haxe.ui.components.Label;
+import haxe.ui.components.NumberStepper;
+import haxe.ui.components.Stepper;
+import haxe.ui.containers.menus.Menu;
+import haxe.ui.containers.menus.MenuBar;
+import haxe.ui.containers.menus.MenuItem;
+import haxe.ui.containers.menus.MenuSeparator;
+import haxe.ui.core.Screen;
+import haxe.ui.events.Events;
+import haxe.ui.events.MouseEvent;
+import haxe.ui.events.UIEvent;
+
+import haxe.ui.Toolkit;
+
+/**
+ * ...
+ * @author Kaelan
+ */
+class MenuMain extends Object 
+{
+	
+	public var menubar:MenuBar;
+		public var fileMenu:Menu;
+			public var newItem:MenuItem;
+			public var saveItem:MenuItem;
+			public var loadItem:MenuItem;
+			//=========================
+			public var connectItem:MenuItem;
+			public var hostItem:MenuItem;
+		public var viewMenu:Menu;
+			public var textMenu:Menu;
+				public var trackNameCheck:CheckBox;
+				public var timeStampCheck:CheckBox;
+				public var speedometerCheck:CheckBox;
+				public var lineCountCheck:CheckBox;
+				public var frameRateCheck:CheckBox;
+			public var gridSizeStepper:NumberStepper;
+	
+	public function toggle()
+	{
+		this.visible = !this.visible;
+	}
+	
+	public function new(?parent:Object) 
+	{
+		super(parent);
+		
+		Toolkit.theme = "dark";
+		Toolkit.init({
+			root: Main.rootScene
+		});
+		
+		menubar = new MenuBar();
+		Screen.instance.addComponent(menubar);
+		
+		fileMenu = new Menu();
+		fileMenu.text = "File";
+		
+		newItem = new MenuItem();
+		newItem.text = "New Track";
+		
+		saveItem = new MenuItem();
+		#if js
+		saveItem.text = "Save Track to file...";
+		#else
+		saveItem.text = "Save Track...";
+		#end
+		
+		loadItem = new MenuItem();
+		loadItem.text = "Load Track";
+		loadItem.onClick = function(event:MouseEvent)
+		{
+			loadTrack();
+		}
+		
+		connectItem = new MenuItem();
+		connectItem.text = "Connect";
+		
+		hostItem = new MenuItem();
+		hostItem.text = "Host";
+		
+		viewMenu = new Menu();
+		viewMenu.text = "View";
+		viewMenu.onClick = function(e:UIEvent)
+		{
+			gridSizeStepper.value = Main.ruler.rulerSize;
+			gridSizeStepper.text = "" + Main.ruler.rulerSize;
+		}
+		
+		textMenu = new Menu();
+		textMenu.text = "Text Info";
+		
+		trackNameCheck = new CheckBox();
+		trackNameCheck.text = "Track name";
+		trackNameCheck.value = true;
+		
+		timeStampCheck = new CheckBox();
+		timeStampCheck.text = "Time stamp";
+		timeStampCheck.value = true;
+		
+		speedometerCheck = new CheckBox();
+		speedometerCheck.text = "Speedometer";
+		speedometerCheck.value = true;
+		
+		lineCountCheck = new CheckBox();
+		lineCountCheck.text = "Line counts";
+		lineCountCheck.value = true;
+		
+		frameRateCheck = new CheckBox();
+		frameRateCheck.text = "Frame rate";
+		frameRateCheck.value = true;
+		
+		var gridlabel:Label = new Label();
+		gridlabel.text = "Grid size";
+		
+		gridSizeStepper = new NumberStepper();
+		gridSizeStepper.text = "Grid Size";
+		gridSizeStepper.min = 0;
+		gridSizeStepper.max = 0x7FFFFFFF;
+		gridSizeStepper.value = 14;
+		gridSizeStepper.step = 1;
+		gridSizeStepper.width = 200;
+		gridSizeStepper.onChange = function(e:UIEvent)
+		{
+			var value:Null<Float> = Std.parseFloat("" + gridSizeStepper.value);
+			if (value != null)
+			{
+				Main.ruler.rulerSize = Std.int(value);
+			}
+		}
+		
+		menubar.addComponent(fileMenu);
+			fileMenu.addComponent(newItem);
+			fileMenu.addComponent(saveItem);
+			fileMenu.addComponent(loadItem);
+			#if js
+			fileMenu.addComponent(new MenuSeparator());
+			fileMenu.addComponent(connectItem);
+			fileMenu.addComponent(hostItem);
+			#end
+		menubar.addComponent(viewMenu);
+			viewMenu.addComponent(textMenu);
+				textMenu.addComponent(trackNameCheck);
+				textMenu.addComponent(timeStampCheck);
+				textMenu.addComponent(speedometerCheck);
+				textMenu.addComponent(lineCountCheck);
+				textMenu.addComponent(frameRateCheck);
+			viewMenu.addComponent(new MenuSeparator());
+			viewMenu.addComponent(gridlabel);
+			viewMenu.addComponent(gridSizeStepper);
+			viewMenu.addComponent(new MenuSeparator());
+	}
+	
+	public function resize() 
+	{
+		menubar.width = Main.locengine.width;
+	}
+	
+	function loadTrack()
+	{
+		SaveLoad.browseForSave();
+	}
+}
