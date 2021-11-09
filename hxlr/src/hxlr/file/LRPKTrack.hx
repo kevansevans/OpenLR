acac2fb3ac60d6820f44d6f811adf45b1c2c6aec
Kaelan <kbeevans@gmail.com>
2021-11-09 02:08:06 -0700

    LRPK tweaks

--- hxlr/src/hxlr/file/LRPKTrack.hx
+++ hxlr/src/hxlr/file/LRPKTrack.hx
@@ -1,6 +1,7 @@
 package hxlr.file;
 import haxe.io.Bytes;
 import haxe.io.BytesBuffer;
+import hxlr.Constants;
 import hxlr.engine.Grid;
 import hxlr.lines.Floor;
 import hxlr.lines.Accel;
@@ -17,15 +18,21 @@ import hxlr.math.geom.Point;
  */
 enum abstract LumpType(String) to String
 {
-	var LINEBASE;
-	var LINEDECO;
-	var LINEMOD_;
-	var RIDER___;
-	var RIDERMOD;
+	var LINEBASE; //For lines that need to maintain precision
+	var LINEDECO; //For scenery lines where data can be culled
+	var LINESPEC; //Data for special line properties
+	
+	var RIDERDEF; //Minimum rider data needed
+	var RIDERMOD; //Non-Standard info for rider
+	
+	var TRACKDEF;
 }
 class LRPKTrack 
 {
 	static var sceneryCompression:Bool = true;
+	
+	//To be implemented later
+	static inline var saveVersion:Int = 0;
 	
 	public static function decode(_fileBytes:Bytes)
 	{
@@ -50,14 +57,16 @@ class LRPKTrack
 				{
 					case LINEBASE :
 						
-						var id:Int = data.getInt32(lump.position);
-						var x1:Float = data.getDouble(lump.position + 4);
-						var y1:Float = data.getDouble(lump.position + 12);
-						var x2:Float = data.getDouble(lump.position + 20);
-						var y2:Float = data.getDouble(lump.position + 28);
-						var type:Int = data.get(lump.position + 36);
-						var shifted:Bool = data.get(lump.position + 37) == 1 ? true : false;
-						var limType:Int = data.get(lump.position + 38);
+						var pos:Int = lump.position;
+						
+						var id:Int = data.getInt32(pos);
+						var x1:Float = data.getDouble(pos += 4);
+						var y1:Float = data.getDouble(pos += 8);
+						var x2:Float = data.getDouble(pos += 8);
+						var y2:Float = data.getDouble(pos += 8);
+						var type:Int = data.get(pos += 8);
+						var shifted:Bool = data.get(pos += 1) == 1 ? true : false;
+						var limType:Int = data.get(pos += 1);
 						
 						var line:LineObject = null;
 						switch (type)
@@ -74,6 +83,8 @@ class LRPKTrack
 						lines[id] = line;
 						
 					case LINEDECO :
+						
+						continue;
 						
 						var compressed:Bool = data.get(lump.position) == 1 ? true : false;
 						
@@ -100,21 +111,44 @@ class LRPKTrack
 						var line:Scenery = new Scenery(new Point(x1, y1), new Point(x2, y2));
 						lines.unshift(line);
 						
-					case RIDER___ :
+					case RIDERDEF :
 						
 						var x:Float = data.getDouble(lump.position);
 						var y:Float = data.getDouble(lump.position + 8);
 						
 						Main.riders.addNewRider(new Point(x, y));
 						
+					case TRACKDEF :
+						
+						var pos = lump.position;
+						
+						var nameLength = data.get(pos);
+						Main.CVAR.trackName = data.getString(pos += 1, nameLength);
+						var authorLength = data.get(pos += nameLength);
+						Main.CVAR.trackAuthor = data.getString(pos += 1, authorLength);
+						Grid.switchPhysicsModel(data.get(pos += authorLength));
+						
 					default :
 				}
 			}
 			
+			var lowest:Float = Math.POSITIVE_INFINITY;
+			
 			for (item in lines)
 			{
+				if (item == null) continue;
+				//Lines are indexed by Line ID, should allow for any order of lines to be saved.
+				//However, this leaves potential gaps, so occasionally a null will return from the iterator.
+				
 				Grid.register(item);
-				Main.canvas.addVisLine(item);
+				
+				if (item.keyList.length == 0) {
+					Main.canvas.addVisLine(item);
+					
+				} else {
+					Grid.unregister(item);
+				}
+				
 			}
 		}
 		
@@ -124,11 +158,7 @@ class LRPKTrack
 	static inline var headerSize:Int = 12;
 	public static function encode():Bytes
 	{
-		
 		var directories:Array<Lump>;
-		var dirLocation:Int = 0;
-		var numOfItems:Int = 0;
-		
 		var header:BytesBuffer = new BytesBuffer();
 		var data:BytesBuffer = new BytesBuffer();
 		var directory:BytesBuffer = new BytesBuffer();
@@ -167,13 +197,25 @@ class LRPKTrack
 			
 			directories.push(lump);
 			data.addBytes(bytes, 0, bytes.length);
+			
+			if (line.special) 
+			{
+				var bytes:Bytes = specLineToBytes(line);
+				var lump = {
+					type : LINESPEC,
+					position : data.length + headerSize
+				}
+				
+				directories.push(lump);
+				data.addBytes(bytes, 0, bytes.length);
+			}
 		}
 		
 		for (rider in Main.riders.riders)
 		{
 			
 			directories.push({
-				type : RIDER___,
+				type : RIDERDEF,
 				position : data.length + headerSize
 			});
 			
@@ -181,6 +223,16 @@ class LRPKTrack
 			
 			data.addBytes(bytes, 0, bytes.length);
 		}
+		
+		directories.push({
+			
+			type : TRACKDEF,
+			position : data.length + headerSize
+			
+		});
+		
+		var trackData = trackDataToByes();
+		data.addBytes(trackData, 0, trackData.length);
 		
 		for (item in directories) 
 		{
@@ -207,6 +259,11 @@ class LRPKTrack
 	
 	static function riderToBytes(_rider:RiderBase):Bytes
 	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		//!!!WARNING!!!
+		//Do not change! This lump does not need it's format changed!
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 		var data:BytesBuffer = new BytesBuffer();
 		
 		data.addDouble(_rider.startPos.x);
@@ -217,6 +274,11 @@ class LRPKTrack
 	
 	static function lineToBytes(_line:LineObject):Bytes
 	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		//!!!WARNING!!!
+		//Do not change! This lump does not need it's format changed!
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 		var data:BytesBuffer = new BytesBuffer();
 		
 		data.addInt32(_line.id);
@@ -233,6 +295,11 @@ class LRPKTrack
 	
 	static function decoLineToBytes(_line:LineObject):Bytes
 	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		//!!!WARNING!!!
+		//Do not change! This lump does not need it's format changed!
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 		var data:BytesBuffer = new BytesBuffer();
 		
 		if (sceneryCompression) {
@@ -253,15 +320,53 @@ class LRPKTrack
 		return data.getBytes();
 	}
 	
-	static function lineSpecialsToBytes(_line:LineObject)
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	//!!!WARNING!!!
+	//What lies ahead is NON-STANDARD data.
+	//Be very VERY careful about modifying these functions!
+	//The safest option is to add new info to parts AFTER what's already defined.
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	static function trackDataToByes():Bytes
 	{
+		var data:BytesBuffer = new BytesBuffer();
 		
+		var name:BytesBuffer = new BytesBuffer();
+		name.addString(Main.CVAR.trackName);
+		
+		data.addByte(Main.CVAR.trackName.length);
+		data.addString(Main.CVAR.trackName);
+		data.addByte(Main.CVAR.trackAuthor.length);
+		data.addString(Main.CVAR.trackAuthor);
+		data.addByte(Main.CVAR.physics);
+		
+		return data.getBytes();
 	}
 	
+	static function specLineToBytes(_line:LineObject):Bytes
+	{
+		var data:BytesBuffer = new BytesBuffer();
+		
+		data.addByte(_line.multiplier);
+		data.addDouble(_line.thickness);
+		data.addByte(_line.grindable == true ? 1 : 0);
+		
+		return data.getBytes();
+	}
 }
 
 typedef Lump =
 {
 	type:String,
 	position:Int,
+}
+
+typedef RiderData =
+{
+	id:Int,
+	pos_x:Float,
+	pos_y:Float,
+	vel_x:Float,
+	vel_y:Float,
+	name:String,
 }
